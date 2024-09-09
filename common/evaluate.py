common: corrected loss function to return total loss and not average of batch averages. Corrected perplexity to show all 5-grams.

common/evaluate.py
@@ -10,7 +10,7 @@ from .train import (
     save_perplexities,
     train,
     evaluate,
-    calculate_perplexity,
+    calculate_nll,
 )
 from NNLM.model import NeuralNetworkLanguageModel
 from RNN.model import RecurrentNeuralNetwork
@@ -26,43 +26,47 @@ def set_perplexity(
 ) -> None:
     # save perplexities for test data
     # todo: can probably make this a single call
-    perplexities = calculate_perplexity(model, loader, device)
-    save_perplexities(perplexities, corpus, file_name)
+    nll_losses = calculate_nll(model, loader, device)
+    assert len(nll_losses) == len(
+        corpus
+    ), "[set_perplexity] nll losses should be same length as corpus"
+    save_perplexities(nll_losses, corpus, file_name)
 
 
 def test_model(model_type: str, path_dir: str) -> None:
     data_path = "./data/Auguste_Maquet.txt"
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    BATCH_SIZE = 32  # reason for choice: arbitrary
 
     train_dataset, val_dataset, test_dataset = prepare_data(data_path)
     os.system("cls || clear")
     print("info -> data prepared!")
     print(f"info -> using device: {device}")
 
-    vocab, embeddings = train_dataset.vocab, train_dataset.embeddings
+    vocab_len = len(train_dataset.vocab)
+    embedding_dim = train_dataset.embeddings.size(1)
 
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=32)
-    test_loader = DataLoader(test_dataset, batch_size=32)
+    train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE)
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE)
+    test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE)
 
-    dropout_rate = 0.2
-    embedding_dim = embeddings.size(1)
+    dropout_rate = 0.3
 
     match model_type:
         case "NNLM":
             model = NeuralNetworkLanguageModel(
-                len(vocab), dropout_rate=dropout_rate, embedding_dim=embedding_dim
+                vocab_len, dropout_rate=dropout_rate, embedding_dim=embedding_dim
             ).to(device)
         case "RNN":
             model = RecurrentNeuralNetwork(
-                len(vocab), dropout_rate=dropout_rate, embedding_dim=embedding_dim
+                vocab_len, dropout_rate=dropout_rate, embedding_dim=embedding_dim
             ).to(device)
         case "Transformer":
-            model = TransformerDecoderLM(len(vocab), embedding_dim).to(device)
+            model = TransformerDecoderLM(vocab_len, embedding_dim).to(device)
         case _:
             raise ValueError(f"model type {model_type} not supported")
 
-    criterion = torch.nn.NLLLoss()
+    criterion = torch.nn.NLLLoss(reduction="sum")
     optimizer = Adam(model.parameters())  # todo: learning rate is a hyper-param here
     print("info -> dropout rate: ", dropout_rate)
 

common/train.py
@@ -1,10 +1,12 @@
 from typing import List
 
+import numpy as np
 import torch
 import torch.utils.data
 import torch.optim
 
 
+# todo: use reduction sum and divide by total number of items
 def train(
     model: torch.nn.Module,
     train_loader: torch.utils.data.DataLoader,
@@ -36,6 +38,8 @@ def evaluate(
     criterion: torch.nn.Module,
     device: torch.device,
 ) -> float:
+    assert test_loader.__len__() > 0, "[evaluate] testing data must be present"
+
     model.eval()
 
     total_loss = 0
@@ -44,14 +48,18 @@ def evaluate(
             context, target = context.to(device), target.to(device)
 
             output = model(context)
-            loss = criterion(output, target)
+            loss = criterion(
+                output, target
+            )  # total loss of the batch, that's why reduction="sum"
 
             total_loss += loss.item()
 
-    return total_loss / len(test_loader)
+    return total_loss / test_loader.__len__()
 
 
-def calculate_perplexity(
+# todo: I'm not certain that the sentence order is maintained. Do some research.
+# a fix would be to convert the dataloader into a batch size of 1.
+def calculate_nll(
     model: torch.nn.Module,
     loader: torch.utils.data.DataLoader,
     device: torch.device,
@@ -63,17 +71,19 @@ def calculate_perplexity(
         for context, target in loader:
             context, target = context.to(device), target.to(device)
 
+            # each of these are (batch_size, item)
             output = model(context)
-            loss = torch.nn.NLLLoss(reduction="mean")(output, target)
+            loss = torch.nn.NLLLoss(reduction="none")(output, target)
             # todo: the default reduction in NLLLoss is mean, but check fi we should add and divide instead
 
-            sentence_perplexities.append(torch.exp(loss).item())
+            # append the perplexities one by one
+            sentence_perplexities.extend(loss.cpu().numpy().tolist())
 
     return sentence_perplexities
 
 
 def save_perplexities(
-    perplexities: List[float], corpus: List[List[str]], file_name: str
+    nll_losses: List[float], corpus: List[List[str]], file_name: str
 ) -> None:
     # get the sentences from the test_loader
     sentences = []
@@ -81,8 +91,8 @@ def save_perplexities(
         sentences.append(" ".join(sentence_data[:-1]) + " -> " + sentence_data[-1])
 
     with open(file_name, "w") as f:
-        for sentence, perplexity in zip(sentences, perplexities):
-            f.write(f"{sentence}\t\t\t\t{perplexity}\n")
+        for sentence, nll_loss in zip(sentences, nll_losses):
+            f.write(f"{sentence}\t\t\t\t{np.exp(nll_loss)}\n")
 
-        average_perplexity = sum(perplexities) / len(perplexities)
+        average_perplexity = np.exp(sum(nll_losses) / len(nll_losses))
         f.write(f"\naverage perplexity: {average_perplexity}")